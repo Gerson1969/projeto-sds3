ea127b73acba68e757233fb007ddcddd8e8ddfa1
Gerson1969 <gersondalto@gmail.com>
2021-05-07 17:08:34 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Chart from 'react-apexcharts';
 import { BASE_URL } from 'utils/requests';
 import { SaleSum } from 'types/Sale';
+import { useEffect, useState } from 'react';
 
 type ChartData = {
     labels: string[],
@@ -11,16 +12,33 @@ type ChartData = {
 const DonutChart = () => {
 
     //FORMA ERRADA
-    let chartData: ChartData = { labels: [], series: [] };
-
-    //aqui é crase, não é aspas simples
     //a chamada é assincrona e precisa especificar a funçao para rodar quando tiver sucesso
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    // neste formato não da tempo dos dados serem apresentados e o grafico é apresentado vazio
+    //isso também faz entrar em looping infinito, pois cada vez que setChart exetuta o componente vai reinderizar 
+    //e passa por aqui novamente
+    //let chartData: ChartData = { labels: [], series: [] };
+
+    //FORMACERTA
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+    useEffect(() =>{
+        axios.get(`${BASE_URL}/sales/amount-by-seller`) //qui é crase, não é aspas simples
         .then(response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
-            chartData = { labels: myLabels, series: mySeries };
+            setChartData({ labels: myLabels, series: mySeries });
+        }
+        )
+    }, [])
+
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/sales/amount-by-seller`) //qui é crase, não é aspas simples
+        .then(response => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+            setChartData({ labels: myLabels, series: mySeries });
             console.log(chartData);
         }
         )
