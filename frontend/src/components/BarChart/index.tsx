c8d7214b31db7511d79cd619ab28c1bea0e44233
Gerson1969 <gersondalto@gmail.com>
2021-05-07 18:13:43 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,56 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { BASE_URL } from 'utils/requests';
+import { SaleSucess } from 'types/Sale';
+import { round } from 'utils/format';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+type ChartData = {
+    labels: {
+        categories: string[];
+    }
+    series: SeriesData[];
+}
 
 const BarChart = () => {
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []
+            }
+        ]
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/sucess-by-seller`) //qui é crase, não é aspas simples
+            .then(response => {
+                const data = response.data as SaleSucess[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
+                setChartData({
+                    labels: {
+                        categories: myLabels
+                    },
+                    series: [
+                        {
+                            name: "% de sucesso",
+                            data: mySeries
+                        }
+                    ]
+                });
+            }
+            )
+    }, [])
+
     const options = {
         plotOptions: {
             bar: {
@@ -9,21 +59,10 @@ const BarChart = () => {
         },
     };
 
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
     return (
-        <Chart 
-            options={{...options, xaxis: mockData.labels}}
-            series={mockData.series}
+        <Chart
+            options={{ ...options, xaxis: chartData.labels }}
+            series={chartData.series}
             type="bar"
             height="240"
         />
@@ -31,3 +70,4 @@ const BarChart = () => {
 }
 
 export default BarChart;
+
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -16,6 +16,7 @@ const DonutChart = () => {
     // neste formato não da tempo dos dados serem apresentados e o grafico é apresentado vazio
     //isso também faz entrar em looping infinito, pois cada vez que setChart exetuta o componente vai reinderizar 
     //e passa por aqui novamente
+    //useEffect resolve o problema e só roda novamente quando os dados forem diferentes
     //let chartData: ChartData = { labels: [], series: [] };
 
     //FORMACERTA
@@ -31,17 +32,6 @@ const DonutChart = () => {
         }
         )
     }, [])
-
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`) //qui é crase, não é aspas simples
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-            setChartData({ labels: myLabels, series: mySeries });
-            console.log(chartData);
-        }
-        )
 
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
